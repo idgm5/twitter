e2c421c8f99e38161cc5b93ca308451ec75e0cb0
idgm5 <idgm@pm.me>
2020-04-19 18:36:10 -0500

    Add gravatar profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   acts_as_voter
+  include Gravtastic
+  gravtastic :secure => true,
+             :filetype => :gif,
+             :size => 250
 end
