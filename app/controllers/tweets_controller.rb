dc119ddd87094d0cfe54683c0b49a753b7e95ddd
idgm5 <idgm@pm.me>
2020-04-19 15:53:22 -0500

    Fix before_action error

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,7 +4,6 @@ class TweetsController < ApplicationController
   end
 
   def create
-    before_action :authenticate_user!
     @tweet = Tweet.new(tweet_params)
     @tweet.user_id = current_user.id
     if @tweet.save
@@ -15,21 +14,18 @@ class TweetsController < ApplicationController
   end
 
   def like
-    before_action :authenticate_user!
     @tweet = Tweet.find(params[:id])
     @tweet.liked_by current_user
     redirect_to '/'
   end
 
   def dislike
-    before_action :authenticate_user!
     @tweet = Tweet.find(params[:id])
     @tweet.disliked_by current_user
     redirect_to '/'
   end
 
   def destroy
-    before_action :authenticate_user!
     @tweet = Tweet.find(params[:id])
     @tweet.destroy
     redirect_to '/', :notice => "Your tweet has been deleted"
