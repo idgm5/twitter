9992f6794ccd71d42f403bfdadbebe4606620c1f
idgm5 <idgm@pm.me>
2020-04-19 16:18:14 -0500

    Add welcome message, change img logo for icon, fix new tweet bug

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,6 +3,10 @@ class TweetsController < ApplicationController
   @tweets = Tweet.all
   end
 
+  def new
+  @tweet = Tweet.new
+  end
+  
   def create
     @tweet = Tweet.new(tweet_params)
     @tweet.user_id = current_user.id
