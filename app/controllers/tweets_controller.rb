e42a208aebdc08340d1ca921700df59fa734e1e1
idgm5 <idgm@pm.me>
2020-04-19 15:50:45 -0500

    Made homepage accesible to non-users

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,41 +1,40 @@
 class TweetsController < ApplicationController
-  before_action :authenticate_user!
   def index
   @tweets = Tweet.all
   end
 
-  def new
-  @tweet = Tweet.new
-  end
-
   def create
-  @tweet = Tweet.new(tweet_params)
-  @tweet.user_id = current_user.id
-  if @tweet.save
-      redirect_to '/tweets#index'
-  else
-    render 'new'
-  end
+    before_action :authenticate_user!
+    @tweet = Tweet.new(tweet_params)
+    @tweet.user_id = current_user.id
+    if @tweet.save
+        redirect_to '/tweets#index'
+    else
+      render 'new'
+    end
   end
 
   def like
-  @tweet = Tweet.find(params[:id])
-  @tweet.liked_by current_user
-  redirect_to '/'
+    before_action :authenticate_user!
+    @tweet = Tweet.find(params[:id])
+    @tweet.liked_by current_user
+    redirect_to '/'
   end
 
   def dislike
+    before_action :authenticate_user!
     @tweet = Tweet.find(params[:id])
     @tweet.disliked_by current_user
     redirect_to '/'
   end
 
   def destroy
+    before_action :authenticate_user!
     @tweet = Tweet.find(params[:id])
     @tweet.destroy
     redirect_to '/', :notice => "Your tweet has been deleted"
   end
-  
+
   private
   def tweet_params
     params.require(:tweet).permit(:body)
